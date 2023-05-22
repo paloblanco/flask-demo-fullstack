dcd8b7b23f9ce3221b3e429447d3f8a073fdefbf
Rocco Panella <45581243+paloblanco@users.noreply.github.com>
2023-05-21 22:22:02 -0400

    initial commit

---
+++ static/js/data.js
@@ -0,0 +1,43 @@
+let data = {
+    australia: {
+        "Domestic Health": 84241,
+        "Education": 68160,
+        "Final Consumption": 247409,
+        "Research and Development": 24918
+    },
+
+    brazil: {
+        "Domestic Health": 81832,
+        "Education": 130424,
+        "Final Consumption": 416043,
+        "Research and Development": 22573
+    },
+
+    uk: {
+        "Domestic Health": 207917,
+        "Education": 144950,
+        "Final Consumption": 498433,
+        "Research and Development": 44736
+    },
+
+    mexico: {
+        "Domestic Health": 32947,
+        "Education": 52362,
+        "Final Consumption": 134626,
+        "Research and Development": 3806
+    },
+
+    singapore: {
+        "Domestic Health": 7312,
+        "Education": 9504,
+        "Final Consumption": 34940,
+        "Research and Development": 6608
+    },
+
+    southAfrica: {
+        "Domestic Health": 15215,
+        "Education": 21368,
+        "Final Consumption": 72704,
+        "Research and Development": 2909
+    }
+};
---
+++ static/js/plots.js
@@ -0,0 +1,66 @@
+// Create an array of each country's numbers
+let australia = Object.values(data.australia);
+let brazil = Object.values(data.brazil);
+let uk = Object.values(data.uk);
+let mexico = Object.values(data.mexico);
+let singapore = Object.values(data.singapore);
+let southAfrica = Object.values(data.southAfrica);
+
+// Create an array of category labels
+let labels = Object.keys(data.australia);
+
+// Display the default plot
+function init() {
+  let data = [{
+    values: australia,
+    labels: labels,
+    type: "pie"
+  }];
+
+  let layout = {
+    height: 600,
+    width: 800
+  };
+
+  Plotly.newPlot("pie", data, layout);
+}
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  let dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a letiable
+  let dataset = dropdownMenu.property("value");
+  // Initialize an empty array for the country's data
+  let data = [];
+
+  if (dataset == 'australia') {
+      data = australia;
+  }
+  else if (dataset == 'brazil') {
+      data = brazil;
+  }
+  else if (dataset == 'uk') {
+      data = uk;
+  }
+  else if (dataset == 'mexico') {
+    data = mexico;
+  }
+  else if (dataset == 'singapore') {
+      data = singapore;
+  }
+  else if (dataset == 'southAfrica') {
+    data = southAfrica;
+  }
+// Call function to update the chart
+  updatePlotly(data);
+}
+
+// Update the restyled plot's values
+function updatePlotly(newdata) {
+  Plotly.restyle("pie", "values", [newdata]);
+}
+
+init();
