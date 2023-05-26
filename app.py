a277448d568c7dfed9a598b0b4ba985fc2083bef
Rocco Panella <45581243+paloblanco@users.noreply.github.com>
2023-05-26 11:03:21 -0400

    basic app working

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import pandas as pd
 
 from flask import Flask, jsonify, render_template
 
@@ -59,6 +60,26 @@ def names():
     return jsonify(all_names)
 
 
+@app.route("/api/v1.0/passengersbyclass")
+def passengers_by_class():
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, 
+                            Passenger.sex, Passenger.pclass,
+                            Passenger.survived).all()
+
+    session.close()
+
+    df = pd.DataFrame(results)
+    results = {}
+    results["Survived"] = df[df['survived']==1].groupby(['pclass'])['name'].count().to_dict()
+    results["Died"] = df[df['survived']==0].groupby(['pclass'])['name'].count().to_dict()
+    print(results)
+    return jsonify(results)
+
+
 @app.route("/api/v1.0/passengers")
 def passengers():
     # Create our session (link) from Python to the DB
@@ -66,17 +87,21 @@ def passengers():
 
     """Return a list of passenger data including the name, age, and sex of each passenger"""
     # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    results = session.query(Passenger.name, Passenger.age, 
+                            Passenger.sex, Passenger.pclass,
+                            Passenger.survived).all()
 
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_passengers
     all_passengers = []
-    for name, age, sex in results:
+    for name, age, sex, pclass, survived in results:
         passenger_dict = {}
         passenger_dict["name"] = name
         passenger_dict["age"] = age
         passenger_dict["sex"] = sex
+        passenger_dict["class"] = pclass
+        passenger_dict["survived"] = survived
         all_passengers.append(passenger_dict)
 
     return jsonify(all_passengers)
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,66 +1,39 @@
-// Create an array of each country's numbers
-let australia = Object.values(data.australia);
-let brazil = Object.values(data.brazil);
-let uk = Object.values(data.uk);
-let mexico = Object.values(data.mexico);
-let singapore = Object.values(data.singapore);
-let southAfrica = Object.values(data.southAfrica);
+// Grab data
+let apiTitanic = "/api/v1.0/passengersbyclass";
 
-// Create an array of category labels
-let labels = Object.keys(data.australia);
-
-// Display the default plot
 function init() {
-  let data = [{
-    values: australia,
-    labels: labels,
-    type: "pie"
-  }];
+  d3.json(apiTitanic).then(function(data) {
+      let filter = "Survived"
+      data = data['Survived']
+      console.log(data);
+      let dataPlot = [{
+        x: Object.keys(data),
+        y: Object.values(data),
+        type: 'bar'
+      }]
+      Plotly.newPlot("plot", dataPlot);
+    });
+  }
 
-  let layout = {
-    height: 600,
-    width: 800
-  };
-
-  Plotly.newPlot("pie", data, layout);
+// Function called by DOM changes
+function refreshPlot() {
+  let dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a letiable
+  let filter = dropdownMenu.property("value");
+  // Call function to update the chart
+  d3.json(apiTitanic).then(function(data) {
+    data = data[filter]
+    console.log(data);
+    let dataPlot = [{
+      x: Object.keys(data),
+      y: Object.values(data),
+      type: 'bar'
+    }]
+    Plotly.newPlot("plot", dataPlot);
+  });
 }
 
 // On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change", getData);
-
-// Function called by DOM changes
-function getData() {
-  let dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a letiable
-  let dataset = dropdownMenu.property("value");
-  // Initialize an empty array for the country's data
-  let data = [];
-
-  if (dataset == 'australia') {
-      data = australia;
-  }
-  else if (dataset == 'brazil') {
-      data = brazil;
-  }
-  else if (dataset == 'uk') {
-      data = uk;
-  }
-  else if (dataset == 'mexico') {
-    data = mexico;
-  }
-  else if (dataset == 'singapore') {
-      data = singapore;
-  }
-  else if (dataset == 'southAfrica') {
-    data = southAfrica;
-  }
-// Call function to update the chart
-  updatePlotly(data);
-}
-
-// Update the restyled plot's values
-function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
-}
+d3.selectAll("#selDataset").on("change", refreshPlot);
 
 init();
